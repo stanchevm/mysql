1eb10b72cb7a1044a6df0d32192d3646f196259e
stanchevm <stanchevm@yahoo.com>
2017-03-28 15:04:12 +0300

    addind file

--- insert.sql
+++ insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO testdb.persons VALUES ('Miro', 15, 'Sofia');
 INSERT INTO testdb.persons VALUES ('Emo', 13, 'Varna');
 INSERT INTO testdb.persons VALUES ('Toshko', 22, 'Pernik');
+INSERT INTO testdb.persons VALUES ('Tony', 22, 'Varna');
