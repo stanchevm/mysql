445266c33fa0734f551a5529282f44199abd0df8
stanchevm <stanchevm@yahoo.com>
2017-03-28 14:32:52 +0300

    fixed insert

--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO person VALUES ('Miro', 15, 'Sofia');
-INSERT INTO person VALUES ('Emo', 13, 'Varna');
-INSERT INTO person VALUES ('Toshko', 22, 'Pernik');
+INSERT INTO testdb.persons VALUES ('Miro', 15, 'Sofia');
+INSERT INTO testdb.persons VALUES ('Emo', 13, 'Varna');
+INSERT INTO testdb.persons VALUES ('Toshko', 22, 'Pernik');
