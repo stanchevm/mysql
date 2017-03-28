300d535d8755243c63a5da24b2a08bc1209a6b0c
stanchevm <stanchevm@yahoo.com>
2017-03-28 14:11:05 +0300

    changed table.sql

--- table.sql
+++ table.sql
@@ -1,9 +1,4 @@
-IF db_id('mirodb') IS NULL
-   CREATE DATABASE mirodb
-
-GO
-
-CREATE TABLE mirodb.persons (
+CREATE TABLE testdb.persons (
   'name' varchar(50),
   'age' int,
   'city' varchar(255)
